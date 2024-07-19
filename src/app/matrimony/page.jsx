f829e9213679b5f69a55378291265040a0223ba8
enemuo <francenemuo@gmail.com>
2024-07-19 11:24:18 +0100

    feat: create pages for different routes

---
+++ src/app/matrimony/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RegistryPage() {
+  return (
+    <div>RegistryPage</div>
+  )
+}
---
+++ src/app/our-story/layout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StoryLayout({children}) {
+  return (
+    <div>{children}</div>
+  )
+}
