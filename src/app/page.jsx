ce1299b8353b0ec9f8cfe1b4b9f623824c164d3c
BlarQ <collinsogunlala@gmail.com>
2024-07-23 09:40:46 +0100

    add: slidetwo component
    Time should be adjusted

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,16 @@
 import Body from "./components/SlideOne";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
+import SlideOne from "./components/SlideOne";
+import SlideTwo from "./components/SlideTwo";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <HomePage />
-      <Body />
+      <SlideOne />
+      <SlideTwo />
       
     </main>
   );
