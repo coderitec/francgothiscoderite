72c31dc22fc8677e74cb0a2767eb3b63f3b34dbe
enemuo <francenemuo@gmail.com>
2024-07-29 06:48:38 +0100

    fix: modify navbar on mobile

--- src/app/components/MobileNav.jsx
+++ src/app/components/MobileNav.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 export default function MobileNav() {
   return (
-    <nav className='relative flex flex-col items-center justify-between space-y-5 shadow-md px-10 py-5 rounded-md font-semibold font-sans'>
+    <nav className='relative flex flex-col items-center justify-between space-y-5 shadow-md px-10 py-5 rounded-md font-semibold font-sans bg-orange-600/70 rounded-b-full w-[400px] h-[50vh] '>
             <Link href='/'>
                 <ul>
                     <li>Matrimony</li>
                 </ul>
             </Link>
-            <Link href='/'>
+            <Link href='/our-story'>
                 <ul>
                     <li>Our Story</li>
                 </ul>
