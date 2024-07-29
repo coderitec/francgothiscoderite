9370f6a70ae45aac37f5520aff51349d6a48a8fc
enemuo <francenemuo@gmail.com>
2024-07-29 07:34:29 +0100

    feat: add prayer section

--- src/app/prayer/page.jsx
+++ src/app/prayer/page.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 export default function PrayerPage() {
   return (
-    <div>
+    <div className='bg-[url(/love.png),radial-gradient(orange,orange)] opacity-75 min-h-screen bg-cover bg-fixed'>
 
       <section>
-        <h2>Prayer to Mary, Mother of Fairest Love</h2>
+        <h2 className='flex items-center justify-center text-white font-sans md:text-4xl text-2xl bg-blue-800/95 md:h-[30vh]'>Prayer to Mary, Mother of Fairest Love</h2>
+      <article className='text-center flex flex-col md:space-y-6 space-y-2 text-2xl md:p-16 p-4 font-serif'>
+
       <p>Mary, Mother of Fairest Love, you are the Mother of Jesus and our Mother. Intercede for us with your Son, as you did at the wedding at Cana. Pray for us, Fairest Mother, to strengthen and bless our families.</p>
 <p>Help those called to marriage to find their spouses and teach them to love with a pure and selfless love.</p>
 <p>Accompany parents in the upbringing and education of their children with unconditional love.</p>
 <p>Help those who have answered the Father&apos;s call to the consecrated life or to apostolic celibacy, to be generous and faithful to their vocations.</p>
 <p>Bring peace, unity, and joy to the homes of families that are broken or burdened. Accompany with the warmth of your presence those who are alone.</p>
-<p>You are the Mater Pulchrae Dilectionis, the Mother of beautiful love. Help us to love as Jesus loves, so we may show each other love full of mercy and forgiveness.</p>
+<p>You are the Mater <em>Pulchrae Dilectionis</em>, the Mother of beautiful love. Help us to love as Jesus loves, so we may show each other love full of mercy and forgiveness.</p>
 <p>May all our love and affections be always faithful, pure, and chaste.</p>
-<p>Dear Mother, be our refuge in our material and spiritual needs, and intercede especially for (here mention your petitions).</p>
+<p>Dear Mother, be our refuge in our material and spiritual needs, and intercede especially for <em>(here mention your petitions)</em>.</p>
 <p>May we grow in love and come to know true peace and joy, now and in the life to come in Heaven.</p>
 <p>Amen.</p>
+      </article>
       </section>
       
     </div>
