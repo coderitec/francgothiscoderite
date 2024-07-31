2e4f492d0bf4bbc87e51db075f55a217a10e727f
enemuo <francenemuo@gmail.com>
2024-07-31 11:18:32 +0100

    fix: adjust height on events

--- src/app/components/MobileNav.jsx
+++ src/app/components/MobileNav.jsx
@@ -4,21 +4,21 @@ import React from 'react'
 export default function MobileNav() {
   return (
     <nav className='relative flex flex-col items-center justify-between space-y-5 shadow-md px-10 py-5 rounded-md font-semibold font-sans bg-orange-600/70 rounded-b-full w-[300px] h-[50vh] '>
-            <Link href='/'>
+            {/* <Link href='/'>
                 <ul>
                     <li>Matrimony</li>
                 </ul>
-            </Link>
+            </Link> */}
             <Link href='/our-story'>
                 <ul>
                     <li>Our Story</li>
                 </ul>
             </Link>
-            <Link href='/'>
+            {/* <Link href='/'>
                 <ul>
                     <li>Registry</li>
                 </ul>
-            </Link>
+            </Link> */}
             <Link href='/support-us'>
                 <ul>
                     <li>Support Us</li>
--- src/app/components/SlideTwo.jsx
+++ src/app/components/SlideTwo.jsx
@@ -4,16 +4,54 @@ import { LuCalendar, LuClock4 } from "react-icons/lu";
 
 export default function SlideTwo() {
     return (
-        <div className='bg-[url(/beach.jpg)] h-[130vh] bg-no-repeat bg-cover'>
-            <div className='backdrop-brightness-75 h-[130vh]'>
+        <div className='bg-[url(/beach.jpg)] bg-no-repeat bg-cover'>
+            <div className='backdrop-brightness-75 min-h-[130vh] pb-8'>
 
-                <div className='flex flex-col items-center justify-center py-32 space-y-4'>
+                <div className='flex flex-col items-center justify-center py-16 space-y-4'>
 
                     <h1 className='font-sans font-bold uppercase text-[#f5f5f5da]'>Our special event</h1>
                     <p className='text-6xl font-bold text-white'>Wedding Events</p>
                 </div>
 
-                <div className='grid md:grid-cols-2 grid-cols-1 md:gap-32 md:px-44 px-4 text-white font-sans text-center'>
+                <div className='grid md:grid-cols-2 grid-cols-1 md:gap-32 gap-4 md:px-44 px-4 text-white font-sans text-center'>
+                    <div className='border-2 border-white rounded-md p-5'>
+                        <div className='border-b-[1px] border-white mx-[10%] py-5'>
+
+                            <h1 className='text-xl uppercase tracking-widest'>Registry</h1>
+                        </div>
+                        
+                        <p className='py-3 text-xl tracking-wider'>Private Event</p>
+
+                        <div className='flex items-center justify-center space-x-8 pb-8'>
+                            <div className='flex flex-col items-center justify-center space-y-2 tracking-wide'>
+                                <LuClock4 className='text-xl'/>
+                                <p><time dateTime="11:00:00">9:00 AM</time></p>
+                            </div>
+                            <div className='flex flex-col items-center justify-center space-y-2 tracking-wide'>
+                                <LuCalendar className='text-xl'/>
+                                <p><time dateTime="2024-10-24">October 9th, 2024</time></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='border-2 border-white rounded-md p-5'>
+                        <div className='border-b-[1px] border-white mx-[10%] py-5'>
+
+                            <h1 className='text-xl uppercase tracking-widest'>Traditional marriage</h1>
+                        </div>
+                        
+                        <p className='py-3 text-xl tracking-wider'>Private Event</p>
+
+                        <div className='flex items-center justify-center space-x-8 pb-8'>
+                            <div className='flex flex-col items-center justify-center space-y-2 tracking-wide'>
+                                <LuClock4 className='text-xl'/>
+                                <p><time dateTime="11:00:00">9:00 AM</time></p>
+                            </div>
+                            <div className='flex flex-col items-center justify-center space-y-2 tracking-wide'>
+                                <LuCalendar className='text-xl'/>
+                                <p><time dateTime="2024-10-24">October 19th, 2024</time></p>
+                            </div>
+                        </div>
+                    </div>
                     <div className='border-2 border-white rounded-md p-5'>
                         <div className='border-b-[1px] border-white mx-[10%] py-5'>
 
