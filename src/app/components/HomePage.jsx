a016bc05e0daf9ed9c1451c3f619bd900a639f16
enemuo <francenemuo@gmail.com>
2024-08-26 17:54:11 +0100

    fix: remove temporal the gallery section

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -5,7 +5,12 @@ export default function HomePage() {
   return (
     <div className='flex flex-col items-center justify-center h-[100vh] gap-y-3'>
         <h1 className='text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-bold text-white mix-blend-difference'>Nassy & Franc</h1>
-        <button className='bg-slate-950 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/support-us'}>support us</Link></button>
+
+        <div className='flex md:flex-row flex-col space-x-4'>
+
+          <button className='bg-slate-950 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/support-us'}>support us</Link></button>
+          <button className='bg-slate-950 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/prayer'}>pray with us</Link></button>
+        </div>
     </div>
   )
 }
