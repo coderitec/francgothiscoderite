9c0347ba37ea11aac5b28984dc1996dd8d4de8dc
enemuo <francenemuo@gmail.com>
2024-08-26 18:04:30 +0100

    feat: add gap

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -6,10 +6,10 @@ export default function HomePage() {
     <div className='flex flex-col items-center justify-center h-[100vh] gap-y-3'>
         <h1 className='text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-bold text-white mix-blend-difference'>Nassy & Franc</h1>
 
-        <div className='flex md:flex-row flex-col space-x-4'>
+        <div className='flex md:flex-row flex-col  gap-4 items-center justify-center'>
 
-          <button className='bg-slate-950 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/support-us'}>support us</Link></button>
-          <button className='bg-slate-950 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/prayer'}>pray with us</Link></button>
+          <button className='bg-slate-950 hover:bg-slate-700 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/support-us'}>support us</Link></button>
+          <button className='bg-slate-950 hover:bg-slate-700 text-white p-3 rounded-md md:text-3xl text-lg capitalize'><Link href={'/prayer'}>pray with us</Link></button>
         </div>
     </div>
   )
