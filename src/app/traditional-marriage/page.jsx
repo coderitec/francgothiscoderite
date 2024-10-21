4b20775c02f8f4ea25db91a641044c416968eb2a
enemuo <francenemuo@gmail.com>
2024-10-21 04:36:50 +0100

    styles: implement mobile responsive design on trad photos

--- src/app/traditional-marriage/page.jsx
+++ src/app/traditional-marriage/page.jsx
@@ -3,6 +3,7 @@ import { tradData } from '../components/data'
 import Image from 'next/image'
 import GratitudeSlide from '../components/GratitudeSlide'
 import Footer from '../components/Footer'
+import styles from './trad.module.css'
 
 const TradPage = () => {
     return (
@@ -11,10 +12,10 @@ const TradPage = () => {
                 <h1 className='text-4xl sm:text-6xl font-bold text-white'>Traditional marriage Photos</h1>
             </div>
 
-            <div className='bg-white min-h-[100vh] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-6 sm:px-28 gap-4 py-14'>
+            <div className={`bg-white min-h-[100vh] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-6 sm:px-28 gap-4 py-14 ${styles.trad}`}>
                 {
                     tradData.map((item, index) => (
-                        <div key={index} className='w-[100%]'>
+                        <div key={index} className={`w-[100%]`}>
                             <Image
                                 className='rounded-lg w-full'
                                 src={item}
